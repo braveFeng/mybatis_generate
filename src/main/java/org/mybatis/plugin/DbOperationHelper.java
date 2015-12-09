a4e130b092ab4e5138b2cfe30ef47fa167a55502
bravefc <jbnz1987@163.com>
2015-12-09 15:57:14 +0800

    commit
    commit

--- src/main/java/org/mybatis/plugin/DbOperationHelper.java
+++ src/main/java/org/mybatis/plugin/DbOperationHelper.java
@@ -47,12 +47,37 @@ public class DbOperationHelper {
 		 }
 		 //获取主键
 		 String pkInfo = "SELECT  DISTINCT c.COLUMN_NAME  FROM  INFORMATION_SCHEMA.TABLE_CONSTRAINTS AS t,  INFORMATION_SCHEMA.KEY_COLUMN_USAGE AS c WHERE  t.TABLE_NAME = c.TABLE_NAME  AND t.TABLE_SCHEMA = '"+detailDto.getDbSchema()+"'  AND t.CONSTRAINT_TYPE = 'PRIMARY KEY' AND t.TABLE_NAME = '" +  detailDto.getTableName().toUpperCase() + "'";
-		 PreparedStatement ps2 = conn.prepareStatement(tableSql);
+		 PreparedStatement ps2 = conn.prepareStatement(pkInfo);
 		 ResultSet pkResult = ps.executeQuery();
 		 StringBuilder builder = new StringBuilder();
 		 if(pkResult.next()){
-			 
+			 if(builder.length() > 0){
+				 builder.append(",");
+			 }
+			 builder.append(getPropertyName(pkResult.getString(1)));
 		 }
+		 detailDto.setPkList(builder.toString());
+	 }
+	 /**
+		 * Description:通过字段名获取属性名
+		 * @param: GetAllFriendRequest
+		 * @return ModelDetailDto
+		 * @Author:bravefc
+		 * @Create Date: 2015-11-05
+		 */
+	 public String getPropertyName(String columnName){
+		 
+		 StringBuilder builder = new StringBuilder();
+		 columnName = columnName.toLowerCase();
+		 String [] array = columnName.split(",");
+		 builder.append(array[0]);
+		 for(int i = 1;i < builder.length(); i++){
+			 if(StringUtils.isBlank(array[i])){
+				 continue;
+			 }
+			 builder.append(array[i].substring(0,1).toUpperCase()).append(array[i].substring(1));
+		 }
+		 return builder.toString();
 	 }
 	 
 }
